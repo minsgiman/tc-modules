9aea45f74cf63bee05878a141c085598136891da
mskang9 <mskang@nhnent.com>
2019-08-14 15:03:14 +0900

    test code 수정

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ let playerObj = new player({
     showTime : true,  //default : false
     coreSwfPath : '/resources/vendor/nvp_web_player/LCP_web_player2016082601.swf',    //default : '/resources/vendor/nvp_web_player/LCP_web_player2016082601.swf'
     skinSwfPath : '/resources/vendor/nvp_web_player/NVP_web_player_skin_tvcast_white.swf',    //default : '/resources/vendor/nvp_web_player/NVP_web_player_skin_tvcast_white.swf'
-    getTokenUrl : '/biz/cameras/token/:serialNo',    //default : '/biz/cameras/token/:serialNo',
+    getTokenUrl : 'http://10.161.240.93:10000/biz/cameras/token/:serialNo',    //default : '/biz/cameras/token/:serialNo',
     playEventHandler : function (event) {
         console.log('playEventHandler - event.status : ' + event.status); //event.status : 'NetStream.Play.Start', 'NetConnection.Connect.Closed', 'finish'
     }
