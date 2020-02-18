39f4f7844a45be541c5c53289ebd8befb02a04f0
mskang9 <mskang@nhnent.com>
2020-02-18 15:34:11 +0900

    ai graph dialog ui 수정

--- src/components/dialogs/ai_graphs_dialog/ai_graphs_dialog.vue
+++ src/components/dialogs/ai_graphs_dialog/ai_graphs_dialog.vue
@@ -4,6 +4,15 @@
             <template slot="content">
                 <div>
                     <h4>{{txtMap.title}}</h4>
+                    <div class="ai_total_wrap">
+                        <span>{{txtMap.manTotal}} </span>
+                        <span class="count_ai emphasis">20</span>
+                        <span>{{txtMap.manCount}}, </span>
+                        <span>{{txtMap.tableTotal}} </span>
+                        <span class="count_ai emphasis">3</span>
+                        <span class="count_ai">/6 </span>
+                        <span>(02.06. 09:41:10)</span>
+                    </div>
                     <div class="ai_date_area">
                         <div class="ai_date_select_wrap">
                             <div class="calendar_wrap">
@@ -12,31 +21,20 @@
                             <div id="calendarContainer" class="calendar_open_area"></div>
                             <span class="date_control_wrap">
                                 <a @click="goPrev" class="prev_btn"></a>
-                                <span class="select_day">{{periodStr}} {{(mode === 'hourly' || mode === 'min') && isToday ? txtMap.today : ''}}</span>
+                                <span class="select_day">{{periodStr}} {{mode === 'hourly' && isToday ? txtMap.today : ''}}</span>
                                 <a @click="goNext" class="next_btn" :class="{disabled: isToday}"></a>
                             </span>
-                            <button class="date_mode_control" @click="toggleMode">{{(mode === 'hourly' || mode === 'min') ? txtMap.weekly : txtMap.daily}}</button>
+                            <button class="date_mode_control" @click="toggleMode">{{mode === 'hourly' ? txtMap.weekly : txtMap.daily}}</button>
                         </div>
                     </div>
                     <div v-show="!isLoading" class="graph_area">
-                        <div class="graph_time_set_wrap" v-show="mode === 'min' || mode === 'hourly' ">
-                            <span class="min" :class="{enable: mode === 'min'}" @click="setGraphTimeUnit(10)">10{{txtMap.min}}</span>
-                            <span class="hour" :class="{enable: mode === 'hourly'}" @click="setGraphTimeUnit(60)">1{{txtMap.hour}}</span>
-                        </div>
-                        <div id="sum_graph" class="sum_graph_wrap" :style="{width: (GRAPH_WIDTH + 'px'), margin: '0 auto', marginTop: mode === 'daily' ? '50px' : '12px'}"></div>
-                        <!--ul class="cam_graph_list" style="text-align:left;">
-                            <li class="cam_graph" v-for="(camera, index) in aiCameraList">
-                                <p class="cam_tit_wrap">
-                                    <img src="/resources/img/ic-toast-camera.svg">
-                                    <span class="cam_name">{{camera.labelName}}</span>
-                                </p>
-                                <div class="cam_graph_wrap">
-                                    <div :id="'cam_graph_' + camera.id"></div>
-                                </div>
-                            </li>
-                        </ul-->
+                        <div id="sum_graph" class="sum_graph_wrap" :style="{width: (GRAPH_WIDTH + 'px'), margin: '30px auto 0'}"></div>
                     </div>
                     <img v-show="isLoading" class="ai_load" src="/resources/img/progress_rolling_blue.svg">
+                    <div v-show="chartData && !isLoading" class="ai_graph_desc_wrap">
+                        <img src="/resources/img/ic-discription-gray.svg">
+                        <span>{{mode === 'hourly' ? txtMap.hourGraphDesc : txtMap.dayGraphDesc}}</span>
+                    </div>
                 </div>
             </template>
         </modal_dialog>
@@ -58,68 +56,6 @@
     const moment: any = (window as any).moment as any;
     const Pikaday: any = (window as any).Pikaday as any;
 
-    function getGraphMaxIndex (array: string[]) {
-        let counter, max = -1, hasSame = false;
-
-        if (!array) {
-            return max;
-        }
-        for (counter = 0; counter < array.length; counter+=1) {
-            if (max === -1) {
-                if (parseInt(array[counter]) > 0) {
-                    max = counter;
-                    hasSame = false;
-                }
-            } else {
-                if (parseInt(array[max]) < parseInt(array[counter])){
-                    max = counter;
-                    hasSame = false;
-                } else if (parseInt(array[max]) === parseInt(array[counter])) {
-                    hasSame = true;
-                }
-            }
-        }
-
-        return hasSame ? -1 : max;
-    }
-
-    function getGraphMinIndex (array: string[]) {
-        let counter, min = -1, hasSame = false;
-
-        if (!array) {
-            return min;
-        }
-        for (counter = 0; counter < array.length; counter+=1) {
-            if (min === -1) {
-                if (parseInt(array[counter]) > 0) {
-                    min = counter;
-                    hasSame = false;
-                }
-            } else {
-                if (parseInt(array[counter]) > 0) {
-                    if (parseInt(array[min]) > parseInt(array[counter])){
-                        min = counter;
-                        hasSame = false;
-                    } else if (parseInt(array[min]) === parseInt(array[counter])) {
-                        hasSame = true;
-                    }
-                }
-            }
-        }
-        return hasSame ? -1 : min;
-    }
-
-    function applyTuiChartMaxMinColor (id: string, maxColor: string, minColor: string, maxIdx: number, minIdx: number) {
-        const svgRects = document.querySelectorAll('#' + id + ' .tui-chart-series-area svg rect'),
-            maxRect = svgRects[maxIdx], minRect = svgRects[minIdx];
-        if (maxRect) {
-            maxRect.setAttribute('fill', maxColor);
-        }
-        if (maxIdx !== minIdx && minRect) {
-            minRect.setAttribute('fill', minColor);
-        }
-    }
-
     @Component({
         components: {
             modal_dialog
@@ -132,6 +68,7 @@
         //@Prop() pRequestCamCharts!: any;
         @Prop() pRequestShopChart!: any;
 
+        chartData: any = null;
         startDate: Date = new Date();
         endDate: Date = new Date();
         mode: string = 'hourly';
@@ -140,8 +77,8 @@
         periodStr: string = '';
         isLoading: boolean = false;
         datePicker: any = null;
-        GRAPH_WIDTH: number = 970;
-        GRAPH_HEIGHT: number = 400;
+        GRAPH_WIDTH: number = 730;
+        GRAPH_HEIGHT: number = 360;
 
         private created() {
             this.periodStr = this.dateFormat(this.endDate, "YYYY.MM.DD (ddd)");
@@ -196,7 +133,7 @@
                 defaultDate: that.endDate ? that.endDate : new Date(),
                 position: 'bottom right',
                 onSelect: function(date: Date) {
-                    if (that.mode === 'hourly' || that.mode === 'min') {
+                    if (that.mode === 'hourly') {
                         that.startDate = new Date(date.valueOf());
                         that.endDate = new Date(date.valueOf());
                         that.periodStr = that.dateFormat(that.endDate, "YYYY.MM.DD (ddd)");
@@ -218,26 +155,18 @@
         requestShopChart() {
             this.isLoading = true;
             this.pRequestShopChart(this.mode, this.startDate, this.endDate).then((response: any) => {
-                    if (this.mode === 'min') {
-                        if (response.peopleArea) {
-                            this.makeAICamLineChart(response.peopleArea, 'sum');
-                        } else {
-                            this.makeAICamLineChart({data: [],
-                                yAxis: {min: 0, max: 0},
-                                xAxis: {items: []}
-                            }, 'sum');
-                        }
+                    if (response.peopleArea) {
+                        this.chartData = response.peopleArea;
+                        this.makeAICamGraph(response.peopleArea, 'sum');
                     } else {
-                        if (response.peopleArea) {
-                            this.makeAICamGraph(response.peopleArea, 'sum');
-                        } else {
-                            this.makeAICamGraph({data: [],
-                                yAxis: {min: 0, max: 0},
-                                xAxis: {items: []}
-                            }, 'sum');
-                        }
+                        this.chartData = null;
+                        this.makeAICamGraph({data: [],
+                            yAxis: {min: 0, max: 0},
+                            xAxis: {items: []}
+                        }, 'sum');
                     }
                 }, (err: any) => {
+                    this.chartData = null;
                 }).finally(() => {
                     this.isLoading = false;
                 });
@@ -328,16 +257,6 @@
             return startMonth + '.' + startDay + ' ~ ' + endMonth + '.' + endDay;
         }
 
-        setGraphTimeUnit(unit: number) {
-            if (unit === 10) {
-                this.mode = 'min';
-            } else {
-                this.mode = 'hourly';
-            }
-            this.requestShopChart();
-            //this.requestCamCharts();
-        }
-
         toggleMode() {
             if (this.mode === 'daily') {
                 this.mode = 'hourly';
@@ -390,8 +309,6 @@
                 }
                 data.categories = chartData.xAxis ? chartData.xAxis.items : [0];
                 data.series = [{name: this.txtMap.man, data: chartData.data ? chartData.data : [0], color}];
-                //const maxIdx = getGraphMaxIndex(data.series[0].data);
-                //const minIdx = getGraphMinIndex(data.series[0].data);
                 const options: any = {
                     theme: "newTheme",
                     chart: {
@@ -409,8 +326,7 @@
                         "suffix": this.txtMap.manCount
                     },
                     xAxis: {
-                        "title": '(' + (this.mode === 'daily' ? this.txtMap.dayCount : this.txtMap.hourCount) + ')',
-                        "labelInterval": this.mode === 'daily' ? 1 : 2
+                        "title": '(' + (this.mode === 'daily' ? this.txtMap.dayCount : this.txtMap.hourCount) + ')'
                     },
                     series: {
                         "hasDot": false,
@@ -431,69 +347,7 @@
                 tui.chart.columnChart(document.getElementById(elementId), data, options);
                 $aiGraphEl.children('.tui-chart').css('margin', "auto");
                 $('#' + elementId + ' .tui-chart-series-custom-event-area').hide();
-                //applyTuiChartMaxMinColor(elementId, '#eb4e61', '#cae01c', maxIdx, minIdx);
             }
-        }
-
-        makeAICamLineChart(chartData: IAiChart, type: string, id?: string) {
-            let categories: any, format: string, yTitle: string, data: any, options: any, elementId: string, container: HTMLElement | null;
-
-            if (type === 'sum') {
-                elementId = 'sum_graph';
-            } else {
-                elementId = 'cam_graph_' + id;
-            }
-            container = document.getElementById(elementId);
-            if (container) {
-                while (container.firstChild) {
-                    container.removeChild(container.firstChild);
-                }
-            }
-            if (chartData.xAxis) {
-                categories = chartData.xAxis.items.filter((item) => {
-                    return !!item;
-                });
-            }
-            yTitle = '(' + this.txtMap.manCount + ')';
-            format = '0';
-            data = {
-                categories: categories ? categories : [0],
-                series: [
-                    {
-                        name: '',
-                        data: chartData.data ? chartData.data : [0]
-                    }
-                ]
-            };
-            options = {
-                chart: {
-                    width: this.GRAPH_WIDTH,
-                    height: this.GRAPH_HEIGHT,
-                    format: format
-                },
-                yAxis: {
-                    title: yTitle,
-                    max: chartData.yAxis ? chartData.yAxis.max : 0,
-                    min: chartData.yAxis ? chartData.yAxis.min : 0
-                },
-                xAxis: {
-                    title: '(' + this.txtMap.hourCount + ')'
-                },
-                legend: {
-                    visible: false
-                },
-                chartExportMenu: {
-                    visible: false
-                }
-            };
-            tui.chart.registerTheme('theme', {
-                series: {
-                    colors: ['#4b96e6']
-                }
-            });
-            options.theme = 'theme';
-            tui.chart.lineChart(container, data, options);
-            $('#' + elementId + ' .tui-chart-series-custom-event-area').hide();
         }
 
         onCloseDialog () {
@@ -509,10 +363,19 @@
 <style lang="less">
     .ai_camera_graph_dlg {
         h4 {
-            font-size: 24px; text-align: left; font-weight:bold; padding:32px 0 20px 32px;
+            font-size: 20px; text-align: left; font-weight:bold; padding:24px 0 24px 24px;
+        }
+        .ai_total_wrap {
+            text-align: left; font-size: 16px; color: #262626; padding:0 0 17px 24px;
+            .count_ai {
+                color: #e94155;
+                &.emphasis {
+                    font-weight: bold;
+                }
+            }
         }
         #sum_graph {
-            width: 100%; height:414px;
+            width: 100%;
         }
         .tui-chart {
             margin: 0 auto;
@@ -523,7 +386,7 @@
             border-bottom: 1px solid #e0e0e0;
         }
         .graph_area {
-            height:528px; overflow-y:auto; overflow-x:hidden;
+            overflow-y:auto; overflow-x:hidden;
             .graph_time_set_wrap {
                 span {
                     cursor: pointer;
@@ -582,6 +445,12 @@
         }
         .date_mode_control {
             border: 1px solid #b2b2b2; width:60px; height:32px; position:absolute; right:24px; top:10px; color:#333333; font-size:13px;
+        }
+        .ai_graph_desc_wrap {
+            padding:20px 0 40px 49px; text-align: left;
+            span {
+                color:#777777; font-size:14px; margin-left:4px;
+            }
         }
     }
 </style>
