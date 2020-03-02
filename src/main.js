e9a77dcf8c0721cfe25bb222fd9e9f0e3da514cc
mskang9 <mskang@nhnent.com>
2020-03-02 13:53:59 +0900

    header 옵션 구현

--- src/main.js
+++ src/main.js
@@ -3,16 +3,21 @@ import player from './components/player/light_webrtc_main';
 let playerObj = new player({
     serialNo: 'BC8AA30000B1',
     elementId : 'playerwrap',
-    startTime : 1581925202062,
-    endTime : 1581925402062,
+    //startTime : 1581925202062,
+    //endTime : 1581925402062,
     cvrJumpInterval : 5000,
     useControl : true, //default: false
     showTime : true,  //default : false
     credentialUrl: 'https://devmedia010.toastcam.com:10090/rtc/credential', //default : /rtc/credential
     candidateUrl: 'https://devmedia010.toastcam.com:10090/rtc/candidate',  //default : /rtc/candidate
     offerUrl: 'https://devmedia010.toastcam.com:10090/rtc/offer',          //default : /rtc/offer
+    getCameraUrl: 'http://10.161.240.93:10000/biz/cameras/:serialNo',
     getTokenUrl : 'http://10.161.240.93:10000/biz/cameras/token/:serialNo',    //default : '/biz/cameras/token/:serialNo',
     getTimelineUrl : 'http://10.161.240.93:10000/biz/cameras/:serialNo/video',
+    requestHeaders : {
+        "testheader": "123",
+        "testheader2": "1234"
+    },
     playEventHandler : function (event) {
         console.log('playEventHandler - event.status : ' + event.status); //event.status : 'stream_connected', 'webrtc_server_error', 'finish', 'webrtc_not_support_browser'
         if (event.status === 'finish') {
